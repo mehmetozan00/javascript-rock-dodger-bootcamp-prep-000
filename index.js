c4c733d14f631bb4fb1ef99587651555ac1db9f8
Ozan Döğüşcü <doguscu_12@hotmail.com>
2018-04-03 17:12:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,21 +12,11 @@ const START = document.getElementById('start')
 
 var gameInterval = null
 
-/**
- * Be aware of what's above this line,
- * but all of your work should happen below.
- */
-
 function checkCollision(rock) {
-  // implement me!
-  // use the comments below to guide you!
-  const top = positionToInteger(rock.style.top)
 
-  // rocks are 20px high
-  // DODGER is 20px high
-  // GAME_HEIGHT - 20 - 20 = 360px;
+  const top = positionToInteger(rock.style.top)
   if (top > 360) {
-        
+
     // Dodger Left-Right Edges
     const dodgerLeftEdge  = positionToInteger(DODGER.style.left);
     const dodgerRightEdge = dodgerLeftEdge + 40;
@@ -47,41 +37,28 @@ function checkCollision(rock) {
 function createRock(x) {
   const rock = document.createElement('div')
 
-  rock.className = 'rock'
-  rock.style.left = `${x}px`
+  rock.className = 'rock';
+  rock.style.left = `${x}px`;
 
-  // Hmmm, why would we have used `var` here?
   var top = 0
-
-  rock.style.top = top
-
-  /**
-   * Now that we have a rock, we'll need to append
-   * it to GAME and move it downwards.
-   */
+  rock.style.top = `${top}px`;
+  GAME.appendChild(rock);
 
 
-  /**
-   * This function moves the rock. (2 pixels at a time
-   * seems like a good pace.)
-   */
   function moveRock() {
-    // implement me!
-    // (use the comments below to guide you!)
-    /**
-     * If a rock collides with the DODGER,
-     * we should call endGame()
-     */
 
-    /**
-     * Otherwise, if the rock hasn't reached the bottom of
-     * the GAME, we want to move it again.
-     */
-
-    /**
-     * But if the rock *has* reached the bottom of the GAME,
-     * we should remove the rock from the DOM
-     */
+    if (checkCollision(rock) === true){
+        endGame();
+    }
+    else if (top < GAME_HEIGHT-20) {
+       top+=4;
+       rock.style.top = `${top}px`
+       window.requestAnimationFrame(moveRock);
+     }
+     else if (top >= GAME_HEIGHT-20) {
+       GAME.removeChild(rock);
+       ROCKS.shift();
+     }
   }
 
   // We should kick of the animation of the rock around here
